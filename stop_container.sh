788aafd5a95dd9ae6cd8a46259541651d30b2633
Varshik <56148265+Varshik-Vallala@users.noreply.github.com>
2024-01-21 16:27:57 -0600

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -4,6 +4,8 @@ set -e
 # Stop the running container (if any)
 # containerID=`docker ps | awk -F" " '{print $1}'`
 # docker rm -f $containerID
-# Stop the running container (if any)
+echo "Starting script"
 containerID=$(docker ps | awk -F" " '{print $1}')
+echo "Container ID: $containerID"
 docker rm -f $containerID
+
